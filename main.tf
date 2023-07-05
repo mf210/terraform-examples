687177d08b9799272c215be3bfa22b2d25a1469b
Mehdi Faresi <mfaresi210@gmail.com>
2023-07-05 04:37:57 -0700

    Resources and Data Sources

--- main.tf
+++ main.tf
@@ -3,3 +3,23 @@ provider "aws" {
     access_key = "ACCESS_KEY"
     secret_key = "SECRET_KEY"
 }
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.0.10.0\24"
+    availability_zone = "eu-west-3a"
+}
+
+data "aws_vpc" "existing_vpc" {
+    default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = "172.31.48.0/20"
+    availability_zone = "eu-west-3a"
+}
