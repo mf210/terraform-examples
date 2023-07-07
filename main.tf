0b344315a7cab3407424fba3c6ae6e5e9108d579
Mehdi Faresi <mfaresi210@gmail.com>
2023-07-06 21:58:41 -0700

    Change resources name

--- main.tf
+++ main.tf
@@ -6,12 +6,19 @@ provider "aws" {
 
 resource "aws_vpc" "development-vpc" {
     cidr_block = "10.0.0.0/16"
+    tags = {
+        Name: "development",
+        vpc_env: "dev"
+    }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
     vpc_id = aws_vpc.development-vpc.id
     cidr_block = "10.0.10.0\24"
     availability_zone = "eu-west-3a"
+    tags = {
+        Name: "subnet-1-dev"
+    }
 }
 
 data "aws_vpc" "existing_vpc" {
@@ -22,4 +29,7 @@ resource "aws_subnet" "dev-subnet-2" {
     vpc_id = data.aws_vpc.existing_vpc.id
     cidr_block = "172.31.48.0/20"
     availability_zone = "eu-west-3a"
+    tags = {
+        Name: "subnet-dev-2"
+    }
 }
