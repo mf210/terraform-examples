9c2a669ba57e8042ef3f0914c075f8d6af58e493
Mehdi Faresi <mfaresi210@gmail.com>
2023-07-07 09:49:48 -0700

    Demo Project: Fetch Amazon Machine Image (AMI) for EC2 Instance

--- demo_project_automate_aws/main.tf
+++ demo_project_automate_aws/main.tf
@@ -108,3 +108,28 @@ resource "aws_security_group" "myapp-sg" {
     }
 }
 ### Create Security Group for Firewall Configuration
+
+
+### Fetch Amazon Machine Image (AMI) for EC2 Instance
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+}
+
+# output "aws_ami_id" {
+#     value = data.aws_ami.latest-amazon-linux-image.id
+# }
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+}
+### Fetch Amazon Machine Image (AMI) for EC2 Instance
