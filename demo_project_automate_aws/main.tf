c57e772da23d68afe38278befb772329be5dbb0f
Mehdi Faresi <mfaresi210@gmail.com>
2023-07-07 09:02:25 -0700

    Demo Project: Use AWS Default Components Default Route Table

--- demo_project_automate_aws/main.tf
+++ demo_project_automate_aws/main.tf
@@ -7,6 +7,8 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 
+
+### Create AWS VPC and Subnet
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -22,16 +24,43 @@ resource "aws_subnet" "myapp-subnet-1" {
         Name: "${var.env_prefix}-subnet-1"
     }
 }
+### Create AWS VPC and Subnet
 
+### Create Internet Gateway
 resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
     tags = {
         Name: "${var.env_prefix}-igw"
     }
 }
+### Create Internet Gateway
 
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
+### Create Rout Table & Associate Subnet with Route Table
+
+# resource "aws_route_table" "myapp-route-table" {
+#     vpc_id = aws_vpc.myapp-vpc.id
+
+#     route {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.myapp-igw.id
+#     }
+
+#     tags = {
+#         Name: "${var.env_prefix}-rtb"
+#     }
+# }
+
+# resource "aws_route_table_association"  "a-rtb-subnet" {
+#     subnet_id = aws_subnet.myapp-subnet-1.id
+#     route_table_id = aws_route_table.myapp-route-table.id
+# }
+### Create Rout Table & Associate Subnet with Route Table
+
+
+### Use AWS Default Components Default Route Table
+
+resource "aws_default_route_table" "main-rtf" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
     route {
         cidr_block = "0.0.0.0/0"
@@ -39,11 +68,8 @@ resource "aws_route_table" "myapp-route-table" {
     }
 
     tags = {
-        Name: "${var.env_prefix}-rtb"
+        Name: "${var.env_prefix}-main-rtb"
     }
 }
 
-resource "aws_route_table_association"  "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}
+### Use AWS Default Components Default Route Table
