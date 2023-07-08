a88b4f0b5b553bc4c48d1e6db1b32b03982e93ea
Mehdi Faresi <mfaresi210@gmail.com>
2023-07-08 00:59:38 -0700

    Modularize demo project: Create the second module - encapsulating the server configurations

--- modularize_demo_project/main.tf
+++ modularize_demo_project/main.tf
@@ -20,89 +20,14 @@ module "myapp-subnet" {
 }
 
 
-### Create Security Group for Firewall Configuration
-resource "aws_security_group" "myapp-sg" {
-    name = "myapp-sg"
+module "myapp-server" {
+    source = "./modules/webserver"
     vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-
-    tags = {
-        Name: "${var.env_prefix}-sg"
-    }
-}
-### Create Security Group for Firewall Configuration
-
-
-### Fetch Amazon Machine Image (AMI) for EC2 Instance
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-
-}
-
-### Create ssh key pair
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
-    public_key = file(var.public_key_location)
-
-}
-### Create ssh key pair
-
-
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
     instance_type = var.instance_type
-
     subnet_id = module.myapp-subnet.subnet.id
-    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-    availability_zone = var.avail_zone
-
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-
-    ### Configure EC2 Server to run entry script and run a Docker container
-    user_data = file("entry-script.sh")
-    # OR you can use:
-    # user_data = <<EOF
-    #                 #!/bin/bash
-    #                 sudo yum update -y && sudo yum install -y docker
-    #                 sudo systemctl start docker
-    #                 sudo usermode -aG docker ec2-user
-    #                 docker run -p 8080:80 nginx
-    #             EOF
-
-    tags {
-        Name: "${var.env_prefix}-server"
-    }
+    avail_zone = var.avail_zone
 }
-### Fetch Amazon Machine Image (AMI) for EC2 Instance
